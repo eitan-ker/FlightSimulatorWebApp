6354cc90681ce2fbe52a471f9eaafdf9ef8c2fe2
menimen <menash9288@gmail.com>
2020-05-03 16:18:08 +0300

    added Flight class and FlightController class

---
+++ FlightControlWeb/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb
+{
+    public class Flight
+    {
+        public DateTime Date_time { get; set; }
+
+        public string FlightID { get; set; }
+
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+        public string Company_name { get; set; }
+        public bool Is_external { get; set; }
+    }
+}
