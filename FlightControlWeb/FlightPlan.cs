c02587715bcfd2f36a95fdc0e8d0079b81c70082
menimen <menash9288@gmail.com>
2020-05-07 14:59:25 +0300

    added FlightPlan and Segment classes

---
+++ FlightControlWeb/FlightPlan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb
+{
+    public class FlightPlan
+    {
+        private int _Passengers;
+        private string _Company_Name;
+        Segment _Initial_Location;
+        private List<Segment> SegmentsList;
+        public FlightPlan(int Passengers, string Company_Name, Segment Initial_Location, List<Segment> list) {
+            this._Passengers = Passengers;
+            this._Company_Name = Company_Name;
+            this._Initial_Location = Initial_Location;
+            this.SegmentsList = list
+        }
+    }
+}
---
+++ FlightControlWeb/Segment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb
+{
+    public class Segment
+    {
+        private double _Longtitude;
+        private double _Latitude;
+        private int _Timespan_seconds;
+        public Segment(double Longtitude, double Latitude, int Timespan_seconds)
+        {
+            this._Latitude = Latitude;
+            this._Longtitude = Longtitude;
+            this._Timespan_seconds = Timespan_seconds;
+        }
+    }
+}
