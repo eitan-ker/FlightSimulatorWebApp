d027520bbe9f0d599df472d3801791f9eb832523
eitan-ker <58944748+eitan-ker@users.noreply.github.com>
2020-05-08 15:44:34 +0300

    started working on server side - for now only demo for testing functionality

---
+++ FlightControlWeb/Controllers/ProductsControllerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightControlWeb.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightControlWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsControllerController : ControllerBase
+    {
+        private IProductManager productsManager = new ProductsManager();
+
+        // GET: api/ProductsController
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        { 
+            return productsManager.GetAllProducts();
+        }
+
+        // GET: api/ProductsController/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ProductsController
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ProductsController/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
