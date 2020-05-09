2ed94d6da85f59762924b144a8cff3df20b1be95
eitan-ker <58944748+eitan-ker@users.noreply.github.com>
2020-05-09 14:53:54 +0300

    updated model

--- FlightControlWeb/Model/FlightManager.cs
+++ FlightControlWeb/Model/FlightManager.cs
@@ -7,5 +7,44 @@ namespace FlightControlWeb.Model
 {
     public class FlightManager : IFlightManager
     {
+        private static List<Flight> flights;
+
+        public void AddFlight(Flight temp)
+        {
+            flights.Add(temp);
+        }
+
+        public void deleteFlight(string id)
+        {
+
+            Flight temp = flights.Where(x => x.FlightID == id).FirstOrDefault();
+            if (temp == null)
+            {
+                throw new Exception("product not found");
+            }
+            flights.Remove(temp);
+        }
+
+        public IEnumerable<Flight> GetAllFlights()
+        {
+            return flights;
+        }
+
+        public Flight GetFlightById(string id)
+        {
+            Flight temp = flights.Where(x => x.FlightID == id).FirstOrDefault();
+            return temp;
+        }
+
+        public void UpdateFlight(Flight temp)
+        {
+            Flight _flight = flights.Where(x => x.FlightID == temp.FlightID).FirstOrDefault();
+            _flight.Date_time = temp.Date_time;
+            _flight.FlightID = temp.FlightID;
+            _flight.Longitude = temp.Longitude;
+            _flight.Latitude = temp.Latitude;
+            _flight.Company_name = temp.Company_name;
+            _flight.Is_external = temp.Is_external;
+        }
     }
 }
--- FlightControlWeb/Model/IFlightManager.cs
+++ FlightControlWeb/Model/IFlightManager.cs
@@ -7,5 +7,10 @@ namespace FlightControlWeb.Model
 {
     interface IFlightManager
     {
+        IEnumerable<Flight> GetAllFlights();
+        Flight GetFlightById(string id);
+        void AddFlight(Flight temp);
+        void UpdateFlight(Flight temp);
+        void deleteFlight(string id);
     }
 }
