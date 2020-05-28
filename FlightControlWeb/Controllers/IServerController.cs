7646d1c4a3c5761fdc473e4c5c87f88d04bdc54b
eitan-ker <58944748+eitan-ker@users.noreply.github.com>
2020-05-28 18:49:44 +0300

    made mock unitest as well

---
+++ FlightControlWeb/Controllers/IServerController.cs
@@ -0,0 +1,17 @@
+﻿using FlightControlWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Controllers
+{
+    public interface IServerController
+    {
+        public List<Server> GetServersList();
+        public Task<IActionResult> AddNewExternalServerToList(Server server);
+        public Task<IActionResult> DeleteExternalServerFromListByID(string ServerID);
+
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,24 +1,37 @@
 using FlightControlWeb;
 using FlightControlWeb.Controllers;
 using FlightControlWeb.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using FlightControlWeb.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestPlatform;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
-    {
-        private readonly IMemoryCache _cache;
+    {      
 
         [TestMethod]
-        public void TestMethod1() // test Server class
+        public void SetFlightTest_TwoSameFlight_ReturnsTrue_() // test Server class
         {
             // Arrange
-           
             Flight test_flight1 = new Flight("testId", 32, 32, 100, "Test_Airways", DateTime.UtcNow, false);
             Flight test_flight2 = new Flight();
 
@@ -37,7 +50,7 @@ namespace UnitTestProject1
                 Assert.Fail();
             }
         }
-        bool isEqualFlights(Flight f1, Flight f2)
+        private bool isEqualFlights(Flight f1, Flight f2)
         {
             if (f1.FlightID.CompareTo(f2.FlightID) != 0) { return false; }
             if (f1.Longitude != f2.Longitude) { return false; }
@@ -48,6 +61,33 @@ namespace UnitTestProject1
             string f2_date = Convert.ToString(f2.Date_time);
             if (f1_date.CompareTo(f2_date) != 0) { return false; }
             if (f1.Is_external != f2.Is_external) { return false; }
+            return true;
+        }
+
+        [TestMethod]
+        public void GetServersList_SameServers_ReturnsTrue()
+        {
+            // Arrange
+            var cache_Test = new MemoryCache(new MemoryCacheOptions());
+            string serJson = File.ReadAllText("./server.json");
+            Server server = JsonConvert.DeserializeObject<Server>(serJson);
+            var Server_Mock = new Mock<IServerController>();
+
+            // Act
+            Server_Mock.Setup(x => x.GetServersList()).Returns(new List<Server>(new List<Server>(1) { server }));
+            List<Server> sl = Server_Mock.Object.GetServersList();
+
+            // Assert
+            Assert.IsTrue(CheckServerTest(server, sl));
+        }
+
+        private bool CheckServerTest(Server server, List<Server> ServerList)
+        {
+            foreach(Server ser in ServerList)
+            {
+                if (server.ServerId.CompareTo(ser.ServerId) != 0) { return false; }
+                if (server.ServerUrl.CompareTo(ser.ServerUrl) != 0) { return false; }
+            }
             return true;
         }
     }
