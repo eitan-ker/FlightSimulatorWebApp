866acaede5f04c26b634bdbefb9e00dbffc08aa7
eitan-ker <58944748+eitan-ker@users.noreply.github.com>
2020-06-01 19:49:24 +0300

    fixed FlightPlan according to convensions

--- FlightControlWeb/Controllers/FlightPlanController.cs
+++ FlightControlWeb/Controllers/FlightPlanController.cs
@@ -16,91 +16,55 @@ namespace FlightControlWeb.Controllers
         {
             this._cache = cache;
         }
-        //POST: api/flightplan
-        /*[Route("flightplan")]
-        [HttpPost("{flightplan}")]
-        // test post method with flightplan object from Postman
-        public IActionResult AddFlightPlan(FlightPlan flightplan, bool isExternal = false)
-        {
-            string flightID = RandomGenerator.RandomString(6, true);
-            Flight curflight = MakeFlight(flightplan, isExternal);
-            //convert to UTC 
-            flightplan.Initial_Location.Date_Time = flightplan.Initial_Location.Date_Time.ToUniversalTime();
-            flightplan.ID = flightID;
-            curflight.FlightID = flightID;
-            if (!_cache.TryGetValue("flightplans", out IList<FlightPlan> flightplans))
-            {
-                flightplans = new List<FlightPlan>
-                {
-                    flightplan
-                };
-                Dictionary<string, Flight> flightsdictionary = new Dictionary<string, Flight>();
-                flightsdictionary.Add(curflight.FlightID, curflight);
-                _cache.Set("flightplans", flightplans);
-                _cache.Set("flights", flightsdictionary);
-            }
-            else
-            {
-                
-                flightplans.Add(flightplan);
-                _cache.TryGetValue("flights", out Dictionary<string, Flight> flights);
-                flights.Add(curflight.FlightID, curflight);
-            }
-
-            return Ok(curflight);
-        }*/
 
 
         [Route("flightplan")]
         [HttpPost("{flightplan}")]
         // post method with flightplan object from Postman, default is false because it comes from postman\file by client
-        public async Task<ActionResult> AddFlightPlans(FlightPlan flightplan, bool isExternal = false)
+        public async Task<ActionResult> AddFlightPlans(FlightPlan flightplan,
+            bool isExternal = false)
         {
-            
-            
-                string flightId = RandomGenerator.RandomString(6, true);//generate a unique ID for flightplan
-                Flight curflight = await Task.Run(() => MakeFlight(flightplan, isExternal));// make flight base on flight plan
-                //convert to UTC 
-                flightplan.Initial_Location.Date_Time = await Task.Run(() => flightplan.Initial_Location.Date_Time.ToUniversalTime());
-                flightplan.ID = flightId;
-                curflight.FlightID = flightId;
-                //check if object with name flightsplans exists in memcache. if not we create one
-                if (!_cache.TryGetValue("flightplans", out IList<FlightPlan> flightplans))
-                {
-                    flightplans = new List<FlightPlan>
-                {
-                    flightplan
-                };
-                    Dictionary<string, Flight> flightsdictionary = new Dictionary<string, Flight>
-                    {
-                        { curflight.FlightID, curflight }
-                    };
-                    //add List in the name of flightplans and dictionary named flights to memcache
-                    _cache.Set("flightplans", flightplans);
-                    _cache.Set("flights", flightsdictionary);
-                }
-                else
-                //flightsplans exists in memcache, therefore flights dictionary exist also so we can add flight to dictionary without hasitation
-                {
-                    flightplans.Add(flightplan);
-                    _cache.TryGetValue("flights", out Dictionary<string, Flight> flights);
+            string flightId = RandomGenerator.RandomString(6, true);//generate a unique ID for flightplan
+            Flight curflight = await Task.Run(() => MakeFlight(flightplan, isExternal));
+            //convert to UTC 
+            flightplan.Initial_Location.Date_Time = await Task.Run(() =>
+            flightplan.Initial_Location.Date_Time.ToUniversalTime());
+            flightplan.ID = flightId;
+            curflight.FlightID = flightId;
+            AddFlightPlan(flightplan, curflight);
+            return Ok();
+        }
+        
+        void AddFlightPlan(FlightPlan flightplan, Flight curflight)
+        {
+            //check if object with name flightsplans exists in memcache. if not we create one
+            if (!_cache.TryGetValue("flightplans", out IList<FlightPlan> flightplans))
+            {
+                flightplans = new List<FlightPlan> { flightplan };
+                Dictionary<string, Flight> flightsdictionary = new Dictionary<string, Flight>
+                    { { curflight.FlightID, curflight } };
+                //add List in the name of flightplans and dictionary named flights to memcache
+                _cache.Set("flightplans", flightplans);
+                _cache.Set("flights", flightsdictionary);
+            }
+            else
+            //flightsplans exists in memcache, therefore flights dictionary exist also so we can 
+            // add flight to dictionary without hasitation
+            {
+                flightplans.Add(flightplan);
+                _cache.TryGetValue("flights", out Dictionary<string, Flight> flights);
                 if (flights != null)
                 {
                     flights.Add(curflight.FlightID, curflight);
-
-                } else
+                }
+                else
                 {
                     Dictionary<string, Flight> newFlight = new Dictionary<string, Flight>()
-                    {
-                        { curflight.FlightID, curflight }
-                    };
+                    { { curflight.FlightID, curflight} };
                     _cache.Set("flights", newFlight);
                 }
-                }
-            
-                return Ok();
+            }
         }
-
 
         //GET: api/flightplan/{id}
         [Route("FlightPlan/{id}")]
@@ -129,15 +93,17 @@ namespace FlightControlWeb.Controllers
             if (_cache.TryGetValue("flightplans", out List<FlightPlan> flightplans))
             {
                 return flightplans.Find(f => f.ID == id);
-             
+
             }
             return null;
         }
         //helper function to create Flight object from FlightPlan object
         private Flight MakeFlight(FlightPlan flightplan, bool isExternal)
         {
-            Flight flight = new Flight(flightplan.ID, flightplan.Initial_Location.Longitude, flightplan.Initial_Location.Latitude,
-                flightplan.Passengers, flightplan.Company_Name, flightplan.Initial_Location.Date_Time, isExternal);
+            Flight flight = new Flight(flightplan.ID, flightplan.Initial_Location.Longitude,
+                flightplan.Initial_Location.Latitude,
+                flightplan.Passengers, flightplan.Company_Name,
+                flightplan.Initial_Location.Date_Time, isExternal);
             return flight;
         }
     }
